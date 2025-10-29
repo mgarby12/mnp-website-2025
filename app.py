7f0d0df8573530f9c96ecdaa4d1f2893c7b6fe74
Melina Garby <mgarby@umich.edu>
2025-10-29 16:27:13 -0400

    all pages to all pages

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask
+from routes import main_routes  # import blueprint
 
 app = Flask(__name__)
+app.register_blueprint(main_routes)  # register routes blueprint
 
-@app.route("/")
-def home():
-    return render_template("index.html", title="Home Page")
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
+
--- routes.py
+++ routes.py
@@ -5,3 +5,19 @@ main_routes = Blueprint('main', __name__)
 @main_routes.route('/')
 def home():
     return render_template('index.html')
+
+@main_routes.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@main_routes.route('/joinus')
+def joinus():
+    return render_template('joinus.html')
+
+@main_routes.route('/faq')
+def faq():
+    return render_template('faq.html')
+
+@main_routes.route('/subteams')
+def subteams():
+    return render_template('subteams.html')
